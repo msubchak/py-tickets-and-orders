9d32cf17df33b95bb5b4986a22eeb01e7f979c0f
Maksym Subchak <donjkee2@gmail.com>
2025-07-04 15:47:55 +0300

    Solution

---
+++ services/order.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket
+
+
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: str=None
+) -> None:
+    with transaction.atomic():
+        user = get_user_model().objects.get(username=username)
+        order = Order.objects.create(user=user)
+        if date:
+            per_date = datetime.fromisoformat(date)
+            order.created_at=per_date
+            order.save()
+        for ticket in tickets:
+            Ticket.objects.create(
+                movie_session_id=ticket["movie_session"],
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"]
+            )
+
+
+def get_orders(username: str=None) -> QuerySet[Order]:
+    with transaction.atomic():
+        if username:
+            return Order.objects.filter(user__username=username)
+        return Order.objects.all()
---
+++ services/user.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+from django.contrib.auth import get_user_model
+
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str | int,
+        email: Optional[str]=None,
+        first_name: Optional[str]=None,
+        last_name: Optional[str]=None,
+) -> User:
+    user = User.objects.create_user(
+        username=username,
+        password=password,
+    )
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    user.save()
+    return user
+
+
+def get_user(user_id: int) -> User:
+    return get_user_model().objects.get(id=user_id)
+
+
+def update_user(
+        user_id: int,
+        username: str=None,
+        password: str | int=None,
+        email: str=None,
+        first_name: str=None,
+        last_name: str=None,
+) -> None:
+    user = get_user_model().objects.get(id=user_id)
+    if username:
+        user.username=username
+    if password:
+        user.set_password(password)
+    if email is not None:
+        user.email=email
+    if first_name is not None:
+        user.first_name=first_name
+    if last_name is not None:
+        user.last_name=last_name
+    user.save()
